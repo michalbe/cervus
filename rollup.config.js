b210e83bfac2ec53d0f5ae492d3e250c53c17588
Michał Budzyński <michal@virtualdesign.pl>
2017-04-15 23:42:00 +0100

    :game_die: babel config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'src/main.js',
   format: 'cjs',
-  plugins: [ resolve({
-    customResolveOptions: {
-      moduleDirectory: 'node_modules'
-    }
-  }) ],
+  plugins: [
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
   // external: ['lodash'], // external shit goes here
   dest: 'dist/bundle.js'
 };
