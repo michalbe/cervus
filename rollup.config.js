e84d605d0c8bc26a647f126da30eac2de2c38cad
Michał Budzyński <michal@virtualdesign.pl>
2017-04-15 23:38:00 +0100

    :pill: rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -3,5 +3,11 @@ import resolve from 'rollup-plugin-node-resolve';
 export default {
   entry: 'src/main.js',
   format: 'cjs',
-  dest: 'dist/bundle.js' // equivalent to --output
+  plugins: [ resolve({
+    customResolveOptions: {
+      moduleDirectory: 'node_modules'
+    }
+  }) ],
+  // external: ['lodash'], // external shit goes here
+  dest: 'dist/bundle.js'
 };
