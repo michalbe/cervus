2fda7e684175caf3be52582877e84f3b0a82b323
Michał Budzyński <michal@virtualdesign.pl>
2017-04-15 02:28:00 +0100

    :fearful: example shaders

---
+++ src/shaders.js
@@ -0,0 +1,20 @@
+const vertex_code =
+  "attribute vec3 aVertex;" +
+  "uniform mat4 uModelView;" +
+  "uniform mat4 uProjection;" +
+  "varying vec4 vColor;" +
+  "void main(void) {" +
+    "gl_Position = uProjection * uModelView * vec4(aVertex, 1.0);" +
+    "vColor = vec4((aVertex.xyz + 1.0) / 2.0, 1.0);" +
+  "}"
+;
+
+const fragment_source =
+  "#ifdef GL_ES\r\n" +
+    "precision mediump float;\r\n" +
+  "#endif\r\n" +
+  "varying vec4 vColor;" +
+  "void main(void) {" +
+    "gl_FragColor = vColor;" +
+  "}"
+;
