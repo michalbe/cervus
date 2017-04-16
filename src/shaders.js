19e059093780d6f1163046b0ad3e8ab307d5f3c9
Michał Budzyński <michal@virtualdesign.pl>
2017-04-16 21:15:00 +0100

    :dolls: add colors to shaders

--- src/shaders.js
+++ src/shaders.js
@@ -2,10 +2,8 @@ const vertex_code =
   "attribute vec3 aVertex;" +
   "uniform mat4 uModelView;" +
   "uniform mat4 uProjection;" +
-  "varying vec4 vColor;" +
   "void main(void) {" +
     "gl_Position = uProjection * uModelView * vec4(aVertex, 1.0);" +
-    "vColor = vec4((aVertex.xyz + 1.0) / 2.0, 1.0);" +
   "}"
 ;
 
@@ -13,7 +11,7 @@ const fragment_code =
   "#ifdef GL_ES\r\n" +
     "precision mediump float;\r\n" +
   "#endif\r\n" +
-  "varying vec4 vColor;" +
+  "uniform vec4 vColor;" +
   "void main(void) {" +
     "gl_FragColor = vColor;" +
   "}"
