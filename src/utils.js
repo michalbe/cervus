3d22841f9e0e9055fffee2630fd727bdedc1e867
Michał Budzyński <michal@virtualdesign.pl>
2017-04-16 22:48:00 +0100

    :hurtrealbad: hex_to_vec in utils

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,19 @@
 const obj_to_vec = (obj) => [obj.x, obj.y, obj.z];
-export { obj_to_vec }
+
+const hex_to_vec = (hex) => {
+  if (hex.charAt(0) === '#') {
+    hex = hex.substr(1);
+  }
+
+  if (hex.length === 3) {
+    hex = hex.split('').map((el) => {
+      return el + '' +  el;
+    });
+  }
+  
+  return hex.match(/.{1,2}/g).map((el) => {
+    return (parseInt(el, 16) / 255);
+  });
+};
+
+export { obj_to_vec, hex_to_vec }
