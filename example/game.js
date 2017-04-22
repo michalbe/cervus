7e6e589f07a7a2bda99a0a448e07a8d2e890c4c3
Michał Budzyński <michal@virtualdesign.pl>
2017-04-22 23:45:00 +0100

    :birthday: Use box shape in example

--- example/game.js
+++ example/game.js
@@ -9,58 +9,6 @@ const game = new Cervus.Game({
 
 // const model = game.model('model.vox');
 
-var vertices = [
-  -1,  1,  1,
-   1,  1,  1,
-   1, -1,  1,
-  -1, -1,  1,
-
-  -1,  1, -1,
-   1,  1, -1,
-   1, -1, -1,
-  -1, -1, -1,
-
-   1,  1,  1,
-   1,  1, -1,
-   1, -1, -1,
-   1, -1,  1,
-
-  -1,  1,  1,
-  -1,  1, -1,
-   1,  1, -1,
-   1,  1,  1,
-
-   1, -1,  1,
-   1, -1, -1,
-  -1, -1, -1,
-  -1, -1,  1,
-
-  -1, -1,  1,
-  -1, -1, -1,
-  -1,  1, -1,
-  -1,  1,  1
-];
-
-var indices = [
-   1,  0,  3,
-   1,  3,  2,
-
-   4,  5,  7,
-   5,  6,  7,
-
-   9,  8, 11,
-   9, 11, 10,
-
-  13, 12, 15,
-  13, 15, 14,
-
-  17, 16, 19,
-  17, 19, 18,
-
-  21, 20, 23,
-  21, 23, 22
-];
-
 // const cubes = 35000;
 //
 // for (let i = 0; i < cubes; i++) {
@@ -70,14 +18,14 @@ var indices = [
 //   cube.position.y = (Math.random()*200)-100;
 //   game.add(cube);
 // }
-const cube2 = new Cervus.Entity({ vertices, indices });
+const cube2 = new Cervus.shapes.Box();
 cube2.position.z = -10;
 cube2.position.x = -5;
 cube2.position.y = 3;
 cube2.color = "#FF00FF";
 game.add(cube2);
 
-const cube = new Cervus.Entity({ vertices, indices });
+const cube = new Cervus.shapes.Box();
 cube.position.z = -10;
 cube.color = "#BADA55";
 game.add(cube);
@@ -85,7 +33,6 @@ game.add(cube);
 
 let dir = 1;
 game.add_frame_listener('cube_rotation', (delta) => {
-  // console.log(delta);
   cube.rotation.y = delta / 1000;
   if (cube2.position.x > 5) {
     dir = -1;
