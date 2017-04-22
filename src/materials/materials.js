0b7fb9b5098208013a3511acfc04fe44b2658348
Michał Budzyński <michal@virtualdesign.pl>
2017-04-22 04:10:00 +0100

    :school_satchel: First try to create a new material

--- src/materials/materials.js
+++ src/materials/materials.js
@@ -1,5 +1,9 @@
 import { Basic } from './basic/basic.js'
+import { Phong } from './phong/phong.js'
 
-const materials = { basic: Basic };
+const materials = {
+  basic: Basic,
+  phong: Phong
+};
 
 export { materials };
