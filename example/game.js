4169dbfa50c82a5597a2ff21614638cb886962cc
Michał Budzyński <michal@virtualdesign.pl>
2017-05-11 03:26:00 +0100

    :flower_playing_cards: change elements material

--- example/game.js
+++ example/game.js
@@ -7,25 +7,19 @@ const game = new Cervus.Game({
   // fps: 1
 });
 
-// const model = game.model('model.vox');
-
-// const cubes = 35000;
-//
-// for (let i = 0; i < cubes; i++) {
-//   let cube = new Cervus.Entity({ vertices, indices });
-//   cube.position.z = -200;
-//   cube.position.x = (Math.random()*300)-150;
-//   cube.position.y = (Math.random()*200)-100;
-//   game.add(cube);
-// }
-const cube2 = new Cervus.shapes.Box();
+const cube2 = new Cervus.shapes.Box({
+  material: 'basic'
+});
 cube2.position.z = -10;
 cube2.position.x = -5;
 cube2.position.y = 3;
 cube2.color = "#FF00FF";
 game.add(cube2);
 
-const cube = new Cervus.shapes.Box();
+const cube = new Cervus.shapes.Box({
+  material: 'phong'
+});
+
 cube.position.z = -10;
 cube.position.x = 3;
 cube.color = "#BADA55";
@@ -41,15 +35,5 @@ game.add_frame_listener('cube_rotation', (delta) => {
     dir = 1;
   }
 
-  // if (cube.position.y > 5) {
-  //   dir2 = -1;
-  // } else if (cube.position.y < -5) {
-  //   dir2 = 1;
-  // }
-
   cube2.position.x += 0.06 * dir;
-  // cube.position.y += 0.06 * dir2;
-  // cube.position.z += 0.06 * dir2;
-  // cube2.scale = {x: 0.5, y:0.5, z: 0.5};
-  // cube2.color = '#' + Math.floor(Math.random() * 250).toString(16) + '' + Math.floor(Math.random() * 250).toString(16) + Math.floor(Math.random() * 250).toString(16);
 });
