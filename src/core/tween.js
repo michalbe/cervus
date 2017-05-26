0c3d860496f7f94230678bcd6996277265ebd306
Michał Budzyński <michal@virtualdesign.pl>
2017-05-27 00:32:00 +0100

    :shoe: repair frame_listeners

--- src/core/tween.js
+++ src/core/tween.js
@@ -1,5 +1,10 @@
 class Tween {
-
+  constructor(options) {
+    this.from = options.from;
+    this.to = options.to;
+    this.time = options.time || 1;
+    this.game = options.game;
+  }
 }
 
 export { Tween }
