a77f31ca87b4ee9b591f0015fa4fcadd3066ccda
Michał Budzyński <michal@virtualdesign.pl>
2017-05-27 15:31:00 +0200

    Tween class foundation

--- core.js
+++ core.js
@@ -1,3 +1,4 @@
 export { Game } from './src/core/game.js';
 export { Entity } from './src/core/entity.js';
+export { Tween } from './src/core/tween.js';
 export { math } from './src/core/math.js';
---
+++ src/core/tween.js
@@ -0,0 +1,5 @@
+class Tween {
+
+}
+
+export { Tween }
