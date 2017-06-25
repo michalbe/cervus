825714c8461df263dcf3f05357fcf3caad8313e0
Michał Budzyński <michal@virtualdesign.pl>
2017-06-25 13:36:00 +0100

    :tongue: update basic material shader to use webgl2

--- src/materials/basic/shader.js
+++ src/materials/basic/shader.js
@@ -1,19 +1,24 @@
-const vertex_code =`
-  attribute vec3 aVertex;
+const vertex_code =`#version 300 es
+
+  in vec3 aVertex;
   uniform mat4 uModelView;
   uniform mat4 uProjection;
+  
   void main(void) {
     gl_Position = uProjection * uModelView * vec4(aVertex, 1.0);
   }
 `;
 
-const fragment_code =`
+const fragment_code =`#version 300 es
   #ifdef GL_ES
     precision mediump float;
   #endif
+
   uniform vec4 uColor;
+  out vec4 outColor;
+  
   void main(void) {
-    gl_FragColor = uColor;
+    outColor = uColor;
   }
 `;
 
