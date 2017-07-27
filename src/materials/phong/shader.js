473ed4292bda480541c4cecda674251b577a7c4f
Michał Budzyński <michal@virtualdesign.pl>
2017-07-28 00:31:00 +0200

    :four_leaf_clover: update phong material shader to use webgl2

--- src/materials/phong/shader.js
+++ src/materials/phong/shader.js
@@ -1,16 +1,17 @@
-const vertex_code =`
-  attribute vec3 aVertex;
-  attribute vec3 aNormal;
+const vertex_code =`#version 300 es
+
+  in vec3 aVertex;
+  in vec3 aNormal;
 
   uniform mat4 uModelView;
   uniform mat4 uProjection;
   uniform mat3 uNormalMatrix;
   uniform vec3 uLightPosition;
 
-  varying float vDiffuse;
-  varying float vSpecular;
-  varying vec4 vPosition;
-  varying vec3 vNormal;
+  out float vDiffuse;
+  out float vSpecular;
+  out vec4 vPosition;
+  out vec3 vNormal;
 
   void main(void) {
     vPosition = uModelView * vec4(aVertex, 1.0);
@@ -32,7 +33,8 @@ const vertex_code =`
   }
 `;
 
-const fragment_code =`
+const fragment_code =`#version 300 es
+
   #ifdef GL_ES
     precision mediump float;
   #endif
@@ -40,15 +42,16 @@ const fragment_code =`
   uniform float uAmbient;
   uniform vec3 uColor;
 
-  varying float vDiffuse;
-  varying float vSpecular;
-  varying vec3 vNormal;
+  in float vDiffuse;
+  in float vSpecular;
+  in vec3 vNormal;
 
+  out vec4 outColor;
   void main(void) {
 
     float light = uAmbient + vDiffuse + vSpecular;
 
-    gl_FragColor = vec4(uColor * light, 0.9);
+    outColor = vec4(uColor * light, 0.9);
   }
 `;
 
