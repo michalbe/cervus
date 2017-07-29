d63ea9c01096be78e6fe7eb77157132906d72230
Michał Budzyński <michal@virtualdesign.pl>
2017-07-29 22:41:00 +0100

    :rage3: shader phong material

--- src/materials/materials.js
+++ src/materials/materials.js
@@ -1,9 +1,11 @@
 import { Basic } from './basic/basic.js'
 import { Phong } from './phong/phong.js'
+import { ShadowPhong } from './shadow-phong/shadow-phong.js'
 
 const materials = {
   basic: Basic,
-  phong: Phong
+  phong: Phong,
+  shadow_phong: ShadowPhong
 };
 
 export { materials };
---
+++ src/materials/shadow-phong/shadow-phong.js
@@ -0,0 +1,74 @@
+import { vertex_code, fragment_code } from './shader.js';
+import { generator_vertex_code, generator_fragment_code } from './map-shader.js';
+import { create_program_object, create_shader_object, gl } from '../../core/context.js';
+
+class ShadowPhong {
+
+  constructor() {
+    this.program = create_program_object(
+      create_shader_object(gl.VERTEX_SHADER, vertex_code),
+      create_shader_object(gl.FRAGMENT_SHADER, fragment_code)
+    );
+
+    if (this.program.error) {
+      console.log(this.program.error); return;
+    }
+
+    this.uniforms = {
+      mProj: gl.getUniformLocation(this.program, 'mProj'),
+      mView: gl.getUniformLocation(this.program, 'mView'),
+      mWorld: gl.getUniformLocation(this.program, 'mWorld'),
+
+      pointLightPosition: gl.getUniformLocation(this.program, 'pointLightPosition'),
+      meshColor: gl.getUniformLocation(this.program, 'meshColor'),
+    };
+
+    this.attribs = {
+      vPos: gl.getAttribLocation(this.program, 'vPos'),
+      vNorm: gl.getAttribLocation(this.program, 'vNorm'),
+    };
+  }
+
+  update(entity) {
+    gl.useProgram(this.program);
+    gl.uniformMatrix4fv(this.uniforms.mProj, gl.FALSE, entity.game.projMatrix);
+    gl.uniformMatrix4fv(this.uniforms.mView, gl.FALSE, entity.game.viewMatrix);
+    gl.uniform3fv(this.uniforms.pointLightPosition, [0, 2, 2]);
+
+    gl.uniformMatrix4fv(
+        this.uniforms.mWorld,
+        gl.FALSE,
+        entity.model_view_matrix
+      );
+      gl.uniform4fv(
+        this.uniforms.meshColor,
+        entity.color_vec
+      );
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, entity.buffers.vertices);
+      gl.vertexAttribPointer(
+        this.attribs.vPos,
+        3, gl.FLOAT, gl.FALSE,
+        0, 0
+      );
+      gl.enableVertexAttribArray(this.attribs.vPos);
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, entity.buffers.normals);
+      gl.vertexAttribPointer(
+
+        this.attribs.vNorm,
+        3, gl.FLOAT, gl.FALSE,
+        0, 0
+      );
+
+      gl.enableVertexAttribArray(this.attribs.vNorm);
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, entity.buffers.indices);
+      gl.drawElements(gl.TRIANGLES, entity.buffers.qty, gl.UNSIGNED_SHORT, 0);
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  }
+}
+
+export { ShadowPhong };
