819106619afa17d069c80f4e9f81533d7c32da96
Michał Budzyński <michal@virtualdesign.pl>
2017-08-05 23:27:00 +0100

    :syringe: groups

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { Game } from './core/game.js';
 import { Entity } from './core/entity.js';
+import { Group } from './core/group.js';
 import { shapes } from './shapes/shapes.js';
 
 export {
   Game,
   Entity,
+  Group,
   shapes
 }
