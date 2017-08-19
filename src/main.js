929e1093fe90206572947147e6044e1a9383b86d
Michał Budzyński <michal@virtualdesign.pl>
2017-08-19 14:57:02 +0200

    :movie_camera: disable shadow-phong material

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import { Game } from './core/game.js';
 import { Entity } from './core/entity.js';
 import { Group } from './core/group.js';
 import { shapes } from './shapes/shapes.js';
+import { vec3, mat4, glMatrix } from 'gl-matrix';
+
+const math = {
+  vec3,
+  mat4,
+  glMatrix
+};
 
 export {
   Game,
   Entity,
   Group,
-  shapes
+  shapes,
+  math
 }
