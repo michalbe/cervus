1c926a76cc361112c11b52f1b29355e52f80751a
Michał Budzyński <michal@virtualdesign.pl>
2017-08-19 18:24:00 +0100

    :watch: vector approach to entity rendering

--- src/core/entity.js
+++ src/core/entity.js
@@ -5,25 +5,62 @@ import { materials } from '../materials/materials.js';
 
 class Entity {
   constructor(options = {}) {
+    const look_at = math.vec3.from_values(0, -1, 1.85);
+    const up = math.vec3.from_values(0, 0, 1);
+
     this.position = options.position || zero_vector.slice();
     this.rotation = options.rotation || zero_vector.slice();
     this.scale = options.scale || unit_vector.slice();
 
-    this.material = options.material || 'basic';
+    this.material = options.material;
 
     this.color = options.color || [1.0, 1.0, 1.0];
     this.color_opacity = 1.0;
+
+    this.forward = [];
+    this.up = [];
+    this.right = [];
+
+    this.moveable = false;
+
+    this.move_speed = 3.5;
+    this.rotate_speed = 1.5;
 
     this.indices = this.indices || options.indices;
     this.vertices = this.vertices || options.vertices;
     this.normals = this.normals || options.normals;
 
-    this.material_desc = new materials[this.material];
-    this.program = this.material_desc.program;
+    this.material_desc = this.material && new materials[this.material];
+    this.program = this.material_desc && this.material_desc.program;
 
     this.skip = false;
 
-    this.create_buffers();
+    this.dir = {};
+
+    this.dir_desc = {
+      87: 'f',
+      65: 'l',
+      68: 'r',
+      83: 'b',
+      81: 'u',
+      69: 'd',
+      38: 'r_u',
+      40: 'r_d',
+      39: 'r_r',
+      37: 'r_l'
+    };
+
+    math.vec3.subtract(this.forward, look_at, this.position);
+    math.vec3.cross(this.right, this.forward, up);
+    math.vec3.cross(this.up, this.right, this.forward);
+
+    math.vec3.normalize(this.forward, this.forward);
+    math.vec3.normalize(this.right, this.right);
+    math.vec3.normalize(this.up, this.up);
+
+    if (this.vertices && this.indices && this.normals) {
+      this.create_buffers();
+    }
   }
 
   create_buffers() {
@@ -35,13 +72,100 @@ class Entity {
     }
   }
 
-  update_material() {
-    this.material_desc = new materials[this.material];
+  get_matrix(out) {
+    const look_at_vect = [];
+    math.vec3.add(look_at_vect, this.position, this.forward);
+    math.mat4.look_at(out, this.position, look_at_vect, this.up);
+    return out;
   }
 
-  update() {
+  rotate_rl(rad) {
+    this.rotation[2] += rad;
+    const rightMatrix = math.mat4.create();
+    math.mat4.rotate(rightMatrix, rightMatrix, rad, this.right);
+    math.vec3.transform_mat4(this.forward, this.forward, rightMatrix);
+    this.realign();
+  }
+
+  rotate_ud(rad) {
+    // this.rotation[2] += rad;
+    const rightMatrix = math.mat4.create();
+    math.mat4.rotate(rightMatrix, rightMatrix, rad, this.up);
+    math.vec3.transform_mat4(this.forward, this.forward, rightMatrix);
+    this.realign();
+  }
+
+  realign() {
+    math.vec3.cross(this.right, this.forward, this.up);
+    math.vec3.cross(this.up, this.right, this.forward);
+
+    math.vec3.normalize(this.forward, this.forward);
+    math.vec3.normalize(this.right, this.right);
+    math.vec3.normalize(this.up, this.up);
+  }
+
+  move_f(dist) {
+    math.vec3.scale_and_add(this.position, this.position, this.forward, dist);
+  }
+
+  move_r(dist) {
+    math.vec3.scale_and_add(this.position, this.position, this.right, dist);
+  }
+
+  move_u(dist) {
+    math.vec3.scale_and_add(this.position, this.position, this.up, dist);
+  }
+
+  do_step(tick_length) {
+    if (this.dir.f && !this.dir.b) {
+      this.move_f(tick_length / 1000 * this.move_speed);
+    }
+
+    if (this.dir.b && !this.dir.f) {
+      this.move_f(-tick_length / 1000 * this.move_speed);
+    }
+
+    if (this.dir.r && !this.dir.l) {
+      this.move_r(tick_length / 1000 * this.move_speed);
+    }
+
+    if (this.dir.l && !this.dir.r) {
+      this.move_r(-tick_length / 1000 * this.move_speed);
+    }
+
+    if (this.dir.u && !this.dir.d) {
+      this.move_u(tick_length / 1000 * this.move_speed);
+    }
+
+    if (this.dir.d && !this.dir.u) {
+      this.move_u(-tick_length / 1000 * this.move_speed);
+    }
+
+    if (this.dir.r_r && !this.dir.r_l) {
+      this.rotate_rl(-tick_length / 1000 * this.rotate_speed);
+    }
+
+    if (this.dir.r_l && !this.dir.r_r) {
+      this.rotate_rl(tick_length / 1000 * this.rotate_speed);
+    }
+
+    if (this.dir.r_u && !this.dir.r_d) {
+      this.rotate_ud(-tick_length / 1000 * this.rotate_speed);
+    }
+
+    if (this.dir.r_d && !this.dir.r_u) {
+      this.rotate_ud(tick_length / 1000 * this.rotate_speed);
+    }
+  }
+
+  update(tick_length, game) {
     if (this.skip) {
       return;
+    }
+
+    if (this.movable) {
+
+      this.do_step(tick_length);
     }
 
     const model_view_matrix_from = (this.parent && this.parent.model_view_matrix)
@@ -58,7 +182,7 @@ class Entity {
   }
 
   render() {
-    !this.skip && this.material_desc.render(this);
+    !this.skip && this.material && this.material_desc.render(this);
   }
 
   // generate_shadow_map() {
