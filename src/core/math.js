ecee1ce22ca92de75a558ff3eefb0ff26db339e6
Michał Budzyński <michal@virtualdesign.pl>
2017-08-21 00:55:00 +0100

    :rice_scene: cross & subtract

--- src/core/math.js
+++ src/core/math.js
@@ -1,10 +1,15 @@
 import * as from_values from 'gl-vec3/fromValues';
+import * as subtract from 'gl-vec3/subtract';
+import * as cross from 'gl-vec3/cross';
+
 import * as create from 'gl-mat4/create';
 import * as perspective from 'gl-mat4/perspective';
 
 const math = {
   vec3: {
-    from_values: from_values.default
+    from_values: from_values.default,
+    subtract: subtract.default,
+    cross: cross.default
   },
   mat4: {
     create: create.default,
