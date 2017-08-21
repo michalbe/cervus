0cb453007937300027bff5278e8d78f69b6a6a02
Michał Budzyński <michal@virtualdesign.pl>
2017-08-21 01:04:00 +0100

    :tongue: camera module rewritten with chunked gl-matrix

--- src/core/math.js
+++ src/core/math.js
@@ -1,21 +1,31 @@
 import * as from_values from 'gl-vec3/fromValues';
 import * as normalize from 'gl-vec3/normalize';
 import * as subtract from 'gl-vec3/subtract';
+import * as add from 'gl-vec3/add';
+import * as scale_and_add from 'gl-vec3/scaleAndAdd';
+import * as transform_mat4 from 'gl-vec3/transformMat4';
 import * as cross from 'gl-vec3/cross';
 
 import * as create from 'gl-mat4/create';
 import * as perspective from 'gl-mat4/perspective';
+import * as rotate from 'gl-mat4/rotate';
+import * as look_at from 'gl-mat4/lookAt';
 
 const math = {
   vec3: {
     from_values: from_values.default,
     normalize: normalize.default,
     subtract: subtract.default,
+    add: add.default,
+    scale_and_add: scale_and_add.default,
+    transform_mat4: transform_mat4.default,
     cross: cross.default
   },
   mat4: {
     create: create.default,
-    perspective: perspective.default
+    perspective: perspective.default,
+    rotate: rotate.default,
+    look_at: look_at.default
   },
   to_radian: (a) => a * Math.PI / 180
 };
