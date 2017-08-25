7040d4690b3ef1c7a95aa2c9c33b792ada59b47e
Michał Budzyński <michal@virtualdesign.pl>
2017-08-25 23:36:34 +0200

    separate modules step 1

--- src/materials/materials.js
+++ src/materials/materials.js
@@ -1,5 +1,5 @@
-import { Basic } from './basic/basic.js'
-import { Phong } from './phong/phong.js'
+import { Basic } from './basic.js'
+import { Phong } from './phong.js'
 // import { ShadowPhong } from './shadow-phong/shadow-phong.js'
 
 const materials = {
