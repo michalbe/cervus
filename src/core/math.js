3091146e32e0aa118b65e246abbd55d78c52b808
Staś Małolepszy <stas@mozilla.com>
2017-08-28 01:21:39 +0200

    Add Entity.look_at
    Fix #40.

--- src/core/math.js
+++ src/core/math.js
@@ -13,9 +13,11 @@ import {
 
 import {
   create,
+  invert,
   perspective,
   multiply,
   translate,
+  rotate,
   fromRotationTranslationScale,
   lookAt,
   targetTo
@@ -25,6 +27,8 @@ import {
   create as quat_create,
   multiply as quat_multiply,
   setAxisAngle,
+  fromMat3,
+  normalize as quat_normalize
 } from 'gl-matrix/src/gl-matrix/quat';
 
 import { hex_to_vec } from '../misc/utils.js';
@@ -43,17 +47,29 @@ export const math = {
   },
   mat4: {
     create,
+    invert,
     perspective,
     multiply,
     translate,
+    rotate,
     compose: fromRotationTranslationScale,
     look_at: lookAt,
   },
   quat: {
     create: quat_create,
     multiply: quat_multiply,
+    set_axes,
     set_axis_angle: setAxisAngle,
   },
   to_radian: (a) => a * Math.PI / 180,
   hex_to_vec
 };
+
+// XXX gl-matrix has a bug in quat.setAxes:
+// https://github.com/toji/gl-matrix/issues/234
+// The following implementation changes the order of axes to match our
+// interpretation.
+function set_axes(out, left, up, forward) {
+  const matrix = Float32Array.of(...left, ...up, ...forward);
+  return quat_normalize(out, fromMat3(out, matrix));
+}
