f4d9557cc2c3a406fee1c0bc581af3bc30080fa3
Michał Budzyński <michal@virtualdesign.pl>
2017-08-28 01:21:39 +0200

    Add Entity.look_at
    Fix #40.

--- example/2/game.js
+++ example/2/game.js
@@ -13,11 +13,19 @@ const game = new Cervus.Game({
 game.camera.position = [0, 1, 4];
 game.camera.rotate_rl(Math.PI);
 
+const plane = new Cervus.shapes.Plane({
+  material: Cervus.materials.phong,
+  color: "#eeeeee",
+  scale: [100, 1, 100]
+});
+
+game.add(plane);
+
 const group = new Cervus.Entity();
 const cube = new Cervus.shapes.Box({
   material: Cervus.materials.phong
 });
-cube.color = "#BADA55";
+cube.color = "#bada55";
 cube.scale = [1, 1, 1];
 group.add(cube);
 
