5e495f959e26dc4ab548046862b2d033fd74e0d8
Staś Małolepszy <stas@mozilla.com>
2017-08-28 15:54:34 +0200

    Store the source in the root dir

--- core.js
+++
@@ -1,3 +0,0 @@
-export { default as Game } from './src/core/gam';
-export { default as Entity } from './src/core/entity';
-export * from './src/core/math';
--- shapes.js
+++
@@ -1,1 +0,0 @@
-export * from './src/shapes/index';
