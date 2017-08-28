fb0c36bb4214183cc6c1c1bf5b6939d9b8087f4f
Michał Budzyński <michal@virtualdesign.pl>
2017-08-28 03:31:20 +0200

    :hushed: Working Basic Tween as a base class

--- src/tweens/basic-tween.js
+++ src/tweens/basic-tween.js
@@ -1,30 +1,27 @@
-import { math } from '../core/math.js';
-
 class BasicTween {
   constructor(options) {
-    this.object = options.object;
-    this.property = options.property;
     this.to = options.to;
     this.time = options.time || 1000;
     this.game = options.game;
+    this.object = options.object;
+    this.property = options.property;
   }
 
   do_step(tick, resolve) {
-    const current_tick = (tick - this.first_tick) / this.tick_length;
-
-    const current_step = Math.min(1, current_tick * this.step);
-    const _from = [];
-    math.vec3.lerp(_from, this.from, this.to, current_step);
-    this.object[this.property] = _from;
-    // console.log(_from);
-    if (current_step === 1) {
+    this.current_step = Math.min(
+      1,
+      ((tick - this.first_tick) / this.tick_length) * this.step
+    );
+    this.action();
+    if (this.current_step === 1) {
       this.game.remove_frame_action(this.do_step.bound);
       resolve();
     }
   }
 
+  action() {}
+
   pre_start() {
-    this.from = this.object[this.property].slice();
     this.tick_length = this.game.tick_length;
     this.first_tick = this.game.last_tick;
     this.number_of_ticks = Math.ceil(this.time / this.tick_length);
