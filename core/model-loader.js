7ee5d0a2e4e110047de4b7afa467d9fa3dec8765
Staś Małolepszy <stas@duzodobrze.pl>
2017-09-05 10:15:32 +0200

    Renames data → response, json → data

--- core/model-loader.js
+++ core/model-loader.js
@@ -2,24 +2,24 @@ import { get_scaling } from '../math/mat4';
 
 export async function model_loader(url) {
   const meshes = [];
-  const data = await fetch(url);
+  const response = await fetch(url);
 
-  if (!data.ok) {
-    throw new Error('Model loader error: ' + data.statusText);
+  if (!response.ok) {
+    throw new Error('Model loader error: ' + response.statusText);
   }
 
-  const json = await data.json();
+  const data = await response.json();
 
-  for (const child of json.rootnode.children) {
+  for (const child of data.rootnode.children) {
     if (!child.meshes) {
       throw new Error('Meshes not found.');
     }
 
     for (const mesh of child.meshes) {
       meshes.push({
-        vertices: json.meshes[mesh].vertices,
-        indices: [].concat(...json.meshes[mesh].faces),
-        normals: json.meshes[mesh].normals,
+        vertices: data.meshes[mesh].vertices,
+        indices: [].concat(...data.meshes[mesh].faces),
+        normals: data.meshes[mesh].normals,
         scale: get_scaling([], child.transformation),
         position: [
           child.transformation[3],
