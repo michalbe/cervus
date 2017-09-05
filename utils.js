53462f4edae8d172d36bac63f4ca725179ae11dd
Michał Budzyński <michal@virtualdesign.pl>
2017-09-05 23:52:26 +0200

    Add color utils: hsl_to_rgb, hsv_to_rgb

--- utils.js
+++ utils.js
@@ -1,20 +1,20 @@
-export function hex_to_vec(hex) {
+export function hex_to_rgb(hex) {
   if (hex.charAt(0) === '#') {
     hex = hex.substr(1);
   }
 
   if (hex.length === 3) {
-    hex = hex.split('').map((el) => {
-      return el + '' +  el;
-    });
+    hex = hex.split('').map(
+      el => el + el
+    );
   }
 
-  return hex.match(/.{1,2}/g).map((el) => {
-    return parseFloat((parseInt(el, 16) / 255).toFixed(1));
-  });
+  return hex.match(/.{1,2}/g).map(
+    el => parseFloat((parseInt(el, 16) / 255).toFixed(2))
+  );
 }
 
-export function vec_to_hex(vec) {
+export function rgb_to_hex(vec) {
   return vec.reduce((sum, value) => {
     let val = (~~(value * 255)).toString(16);
 
@@ -24,4 +24,78 @@ export function vec_to_hex(vec) {
 
     return sum + val;
   }, '#');
+}
+
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 1].
+ *
+ * Credit: https://gist.github.com/mjackson/5311256
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+export function hsl_to_rgb(h, s, l) {
+  let r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    let p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [r, g, b];
+}
+
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 1].
+ *
+ * Credit: https://gist.github.com/mjackson/5311256
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+export function hsv_to_rgb(h, s, v) {
+  let r, g, b;
+
+  let i = ~~(h * 6);
+  let f = h * 6 - i;
+  let p = v * (1 - s);
+  let q = v * (1 - f * s);
+  let t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+
+  return [r, g, b];
 }
