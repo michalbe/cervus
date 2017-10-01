72b4871172af2c4b3618be81f191c601ec1fab6e
Michał Budzyński <michal@virtualdesign.pl>
2017-10-01 13:07:00 +0200

    :airplane: Add RigidBody component (Fixes #79)

--- _example/4/game.js
+++ _example/4/game.js
@@ -1,5 +1,5 @@
 /* global Cervus */
-const material = new Cervus.materials.WireframeMaterial({
+const material = new Cervus.materials.PhongMaterial({
   requires: [
     Cervus.components.Render,
     Cervus.components.Transform
@@ -15,12 +15,13 @@ const game = new Cervus.core.Game({
   // fps: 1
 });
 
+const physics_world = new Cervus.physics.World();
+
 // By default all entities face the user.
 // Rotate the camera to see the scene.
 const camera_transform = game.camera.get_component(Cervus.components.Transform);
 camera_transform.position = [0, 3, 5];
 camera_transform.rotate_rl(Math.PI);
-// game.camera.keyboard_controlled = true;
 
 const plane = new Cervus.shapes.Plane();
 const plane_transform = plane.get_component(Cervus.components.Transform);
@@ -28,23 +29,33 @@ const plane_render = plane.get_component(Cervus.components.Render);
 plane_transform.scale = [100, 1, 100];
 plane_render.material = material;
 plane_render.color = "#eeeeee";
+plane.add_component(new Cervus.components.RigidBody({
+  world: physics_world,
+  shape: 'box',
+  mass: Infinity
+}));
 game.add(plane);
 
-const cube = new Cervus.shapes.Box();
-const cube_transform = cube.get_component(Cervus.components.Transform);
-const cube_render = cube.get_component(Cervus.components.Render);
-cube_render.material = material;
-cube_render.color = "#00ff00";
-cube_transform.position = [0, 1, -10];
+for (let i = 0; i < 15; i++) {
+  const cube = new Cervus.shapes.Box();
+  const cube_transform = cube.get_component(Cervus.components.Transform);
 
-const group = new Cervus.core.Entity({
-  components: [
-    new Cervus.components.Transform()
-  ]
+  const cube_render = cube.get_component(Cervus.components.Render);
+  cube_render.material = material;
+  cube_render.color = '#'+Math.floor(Math.random()*1677215).toString(16);
+  cube_transform.position = [
+    0,
+    4 * i,
+    -10
+  ];
+  cube.add_component(new Cervus.components.RigidBody({
+    world: physics_world,
+    shape: 'box',
+    mass: 5
+  }));
+  game.add(cube);
+}
+
+game.on('tick', () => {
+  physics_world.step(1/game.fps);
 });
-game.add(group);
-group.add(cube);
-//
-// game.on('tick', () => {
-//   group.get_component(Cervus.components.Transform).rotate_rl(16/1000);
-// });
