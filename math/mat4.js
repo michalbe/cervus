5c4a41d9e626b132db8d3d2c0e766986abb429ac
Michał Budzyński <michal@virtualdesign.pl>
2017-10-15 02:00:33 +0200

    :scroll: get_rotation in mat4

--- math/mat4.js
+++ math/mat4.js
@@ -5,5 +5,6 @@ export {
   multiply,
   fromRotationTranslationScale as compose,
   getScaling as get_scaling,
+  getRotation as get_rotation,
   lookAt as look_at
 } from 'gl-matrix/src/gl-matrix/mat4';
